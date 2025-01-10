977214d32ac8dd509f578d46b6deecf334e015e3
0xalkindivv.eth <kindijabi31@gmail.com>
2025-01-10 19:24:15 +0700

    feat: update robots file

--- app/robots.ts
+++ app/robots.ts
@@ -2,40 +2,18 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: [
-          '/api/',
-          '/_next/',
-          '/static/',
-          '/admin/',
-          '/draft/',
-          '/preview/',
-          '/*?*',
-        ],
-      },
-      {
-        userAgent: 'Googlebot-Image',
-        disallow: ['/'],
-        allow: [
-          '/images/*',
-          '*.jpg',
-          '*.jpeg',
-          '*.gif',
-          '*.png',
-          '*.webp',
-          '*.svg',
-        ],
-      },
-      {
-        userAgent: 'Googlebot-News',
-        disallow: ['/'],
-        allow: ['/blog/*'],
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/api/',
+        '/_next/',
+        '/static/',
+        '/admin/',
+        '/draft/',
+        '/preview/',
+      ],
+    },
     sitemap: 'https://alkindivv.site/sitemap.xml',
-    host: 'https://alkindivv.site',
   };
 }
