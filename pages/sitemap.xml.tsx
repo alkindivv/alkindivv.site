22795a32c59e967bf1051cebf945867b366a2dd2
0xalkindivv.eth <kindijabi31@gmail.com>
2025-01-10 18:44:58 +0700

    feat: fix sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -5,12 +5,30 @@ const Sitemap = () => null;
 
 // Fungsi untuk mengenkode karakter khusus XML
 const encodeXMLChars = (str: string) => {
+  if (!str) return '';
   return str
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;')
-    .replace(/'/g, '&apos;');
+    .replace(/'/g, '&apos;')
+    .replace(/[^\x20-\x7E]/g, ''); // Hapus karakter non-ASCII
+};
+
+// Fungsi untuk memvalidasi URL
+const isValidUrl = (url: string) => {
+  try {
+    new URL(url);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+// Fungsi untuk membersihkan URL
+const cleanUrl = (url: string) => {
+  if (!url) return '';
+  return url.trim().replace(/\s+/g, '-');
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
@@ -21,102 +39,107 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
   // Generate sitemap XML with advanced features
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+  <!-- Homepage -->
+  <url>
+    <loc>${baseUrl}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+    <image:image>
+      <image:loc>${baseUrl}/images/ALKINDI-bg.PNG</image:loc>
+      <image:title>${encodeXMLChars('AL KINDI - Law, Technology, and Cryptocurrency')}</image:title>
+    </image:image>
+  </url>
 
-      <!-- Homepage -->
-      <url>
-        <loc>${baseUrl}</loc>
-        <lastmod>${currentDate}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-        <image:image>
-          <image:loc>${baseUrl}/images/ALKINDI-bg.PNG</image:loc>
-          <image:title>${encodeXMLChars('AL KINDI - Law, Technology, and Cryptocurrency')}</image:title>
-        </image:image>
-      </url>
+  <!-- Main Pages -->
+  <url>
+    <loc>${baseUrl}/about</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
 
-      <!-- Main Pages -->
-      <url>
-        <loc>${baseUrl}/about</loc>
-        <lastmod>${currentDate}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.9</priority>
-      </url>
+  <url>
+    <loc>${baseUrl}/blog</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
 
-      <url>
-        <loc>${baseUrl}/blog</loc>
-        <lastmod>${currentDate}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.9</priority>
-      </url>
+  <url>
+    <loc>${baseUrl}/contact</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
 
-      <url>
-        <loc>${baseUrl}/contact</loc>
-        <lastmod>${currentDate}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-      </url>
+  <!-- Category Pages -->
+  ${categories
+    .filter((category) => category.slug)
+    .map(
+      (category) => `
+  <url>
+    <loc>${baseUrl}/blog/${cleanUrl(category.slug)}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+    )
+    .join('\n')}
 
-      <!-- Category Pages -->
-      ${categories
-        .map(
-          (category) => `
-        <url>
-          <loc>${baseUrl}/blog/${category.slug}</loc>
-          <lastmod>${currentDate}</lastmod>
-          <changefreq>weekly</changefreq>
-          <priority>0.8</priority>
-        </url>
-      `
-        )
-        .join('')}
+  <!-- Blog Posts -->
+  ${posts
+    .filter((post) => post.slug && post.category)
+    .map((post) => {
+      const postDate = new Date(post.date);
+      const isRecent =
+        new Date().getTime() - postDate.getTime() < 60 * 24 * 60 * 60 * 1000; // 60 days
 
-      <!-- Blog Posts -->
-      ${posts
-        .map((post) => {
-          const postDate = new Date(post.date);
-          const isRecent =
-            new Date().getTime() - postDate.getTime() <
-            60 * 24 * 60 * 60 * 1000; // 60 days
+      const postUrl = `${baseUrl}/blog/${cleanUrl(post.category)}/${cleanUrl(
+        post.slug
+      )}`;
 
-          return `
-          <url>
-            <loc>${baseUrl}/blog/${post.category}/${post.slug}</loc>
-            <lastmod>${new Date(post.date).toISOString()}</lastmod>
-            <changefreq>${isRecent ? 'daily' : 'weekly'}</changefreq>
-            <priority>${isRecent ? '0.8' : '0.6'}</priority>
-            ${
-              post.featuredImage
-                ? `
-            <image:image>
-              <image:loc>${baseUrl}${post.featuredImage}</image:loc>
-              <image:title>${encodeXMLChars(post.title)}</image:title>
-              <image:caption>${encodeXMLChars(post.excerpt || post.title)}</image:caption>
-            </image:image>
-            `
-                : ''
-            }
-            ${
-              isRecent
-                ? `
-            <news:news>
-              <news:publication>
-                <news:name>AL KINDI</news:name>
-                <news:language>id</news:language>
-              </news:publication>
-              <news:publication_date>${new Date(post.date).toISOString()}</news:publication_date>
-              <news:title>${encodeXMLChars(post.title)}</news:title>
-            </news:news>
-            `
-                : ''
-            }
-          </url>
-        `;
-        })
-        .join('')}
-    </urlset>`;
+      if (!isValidUrl(postUrl)) return '';
+
+      const imageSection = post.featuredImage
+        ? `
+    <image:image>
+      <image:loc>${baseUrl}${cleanUrl(post.featuredImage)}</image:loc>
+      <image:title>${encodeXMLChars(post.title)}</image:title>
+      ${
+        post.excerpt
+          ? `<image:caption>${encodeXMLChars(post.excerpt)}</image:caption>`
+          : ''
+      }
+    </image:image>`
+        : '';
+
+      const newsSection = isRecent
+        ? `
+    <news:news>
+      <news:publication>
+        <news:name>AL KINDI</news:name>
+        <news:language>id</news:language>
+      </news:publication>
+      <news:publication_date>${new Date(post.date).toISOString()}</news:publication_date>
+      <news:title>${encodeXMLChars(post.title)}</news:title>
+    </news:news>`
+        : '';
+
+      return `
+  <url>
+    <loc>${postUrl}</loc>
+    <lastmod>${new Date(post.date).toISOString()}</lastmod>
+    <changefreq>${isRecent ? 'daily' : 'weekly'}</changefreq>
+    <priority>${isRecent ? '0.8' : '0.6'}</priority>${imageSection}${newsSection}
+  </url>`;
+    })
+    .filter(Boolean)
+    .join('\n')}
+</urlset>`;
 
   // Set cache headers for better performance
   res.setHeader('Content-Type', 'text/xml');
