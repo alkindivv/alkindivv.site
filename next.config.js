003519db4f6df1d5f8565281ccf6a695d2c7f903
0xalkindivv.eth <kindijabi31@gmail.com>
2025-02-02 09:58:44 +0700

    feat: added new features

--- next.config.js
+++ next.config.js
@@ -13,28 +13,16 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     unoptimized: true,
   },
-  output: 'export',
   experimental: {
-    optimizeFonts: true,
-    optimizeImages: true,
     scrollRestoration: true,
     workerThreads: true,
     optimizeCss: true,
-    legacyBrowsers: false,
-    browsersListForSwc: true,
   },
   poweredByHeader: false,
   compress: true,
   swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
-  // Optimasi untuk loading
-  onDemandEntries: {
-    // periode halaman di cache
-    maxInactiveAge: 25 * 1000,
-    // jumlah halaman yang di cache
-    pagesBufferLength: 2,
   },
 };
 
