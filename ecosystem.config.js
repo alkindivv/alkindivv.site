e207cffd7c9846963d05f820e47b6956b22d6c03
alkindivv <kindijabi31@gmail.com>
2025-05-13 13:21:20 +0000

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,14 @@ module.exports = {
       args: 'start',
       env: {
         NODE_ENV: 'production',
+        HOST: '0.0.0.0',
         PORT: 9999,
         DATABASE_URL:
           'postgresql://postgres:YvJTiBtbUhGeDdfupTwPWzlPjaAKJoJC@autorack.proxy.rlwy.net:52070/railway',
       },
+      watch: false,
+      instances: 1,
+      exec_mode: 'fork',
     },
   ],
 };
