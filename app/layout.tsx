3464fcf0021fcd6589dd5bfd25284bdd770e93d3
0xalkindivv.eth <kindijabi31@gmail.com>
2025-06-30 11:45:54 +0700

    feat: fix few file

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,45 +109,7 @@ export const metadata: Metadata = {
       'Personal website & blog by AL KINDI – sharing insights on corporate law, technology, blockchain and cryptocurrency.',
     images: ['/images/default.png'],
   },
-  icons: {
-    icon: [
-      '/favicon.ico',
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/android-icon-192x192.png', sizes: '192x192', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-icon-180x180.png', sizes: '180x180', type: 'image/png' },
-      { url: '/apple-icon-152x152.png', sizes: '152x152', type: 'image/png' },
-      { url: '/apple-icon-144x144.png', sizes: '144x144', type: 'image/png' },
-      { url: '/apple-icon-120x120.png', sizes: '120x120', type: 'image/png' },
-      { url: '/apple-icon-114x114.png', sizes: '114x114', type: 'image/png' },
-      { url: '/apple-icon-76x76.png', sizes: '76x76', type: 'image/png' },
-      { url: '/apple-icon-72x72.png', sizes: '72x72', type: 'image/png' },
-      { url: '/apple-icon-60x60.png', sizes: '60x60', type: 'image/png' },
-      { url: '/apple-icon-57x57.png', sizes: '57x57', type: 'image/png' },
-    ],
-    other: [
-      {
-        rel: 'shortcut icon',
-        url: '/favicon-96x96.png',
-        sizes: '96x96',
-        type: 'image/png',
-      },
-      {
-        rel: 'ms-icon',
-        url: '/ms-icon-150x150.png',
-        sizes: '150x150',
-        type: 'image/png',
-      },
-      {
-        rel: 'ms-icon',
-        url: '/ms-icon-310x310.png',
-        sizes: '310x310',
-        type: 'image/png',
-      },
-    ],
-  },
+
   manifest: '/manifest.json',
   other: {
     'geo.region': 'ID',
